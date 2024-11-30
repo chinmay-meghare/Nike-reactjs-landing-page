30b2f6dabf95fa18f9456829351a8b8dfc6d9748
Chinmay Meghare <chinmaym100@gmail.com>
2024-11-30 14:34:32 +0530

    Added Github Actions workflow for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Nike-reactjs-landing-page/',  // Adjust this path to your GitHub Pages URL
   plugins: [react()],
 })

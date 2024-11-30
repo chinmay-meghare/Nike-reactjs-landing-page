78b63bcd1470f07ac4dbe98e1362b7c1c9ad9c2f
Chinmay Meghare <chinmaym100@gmail.com>
2024-11-30 14:55:38 +0530

    Fix deployment configurations

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Nike-reactjs-landing-page/',  // Adjust this path to your GitHub Pages URL
+  base: '/Nike-reactjs-landing-page/',  // This is crucial
   plugins: [react()],
 })
